036f97eba41318e1b808d92d4b4700467d7d7ff9
sergio noriega cantillo <sergionoriegacantillo@gmail.com>
2024-07-27 15:12:23 +0200

    add the logic of tags, now it's missing pass the states to useContext like that thoses states are global

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,74 +1,120 @@
 import React, { useEffect } from "react";
 import { TodoContext } from "../TodoContext";
-import { TodoTag } from "../TodoTag"
-import "./TodoForm.css"
+import { TodoTag } from "../TodoTag";
+import "./TodoForm.css";
 
-function TodoForm(){
+function TodoForm() {
+  const { addTodo, setOpenModal } = React.useContext(TodoContext);
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+  const [newCategoryValue, setNewCategoryValue] = React.useState("");
+  const [CategoriesList,setCategoriesList] = React.useState([])
+  const [isTodoTag, setTodoTag] = React.useState(false);
 
-    const {
-        addTodo,
-        setOpenModal,
-    } = React.useContext(TodoContext);
-    const [newTodoValue, setNewTodoValue] = React.useState("");
-    const [newCategoryValue,setNewCategoryValue] = React.useState("");
-    const [isTodoTag,setTodoTag] = React.useState(false)
-
-    const DetectEvents = (event) =>{
-        if(event.keyCode === 32){
-            setTodoTag(!isTodoTag)
-            console.log("WAS PRESSED THE SPACE KEY")
-        }
+  const DetectEvents = (event) => {
+    if (event.keyCode === 32) {
+      setTodoTag(true);
+      console.log("WAS PRESSED THE SPACE KEY");
     }
+  };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
 
-    const onSubmit = (event) =>{
-        event.preventDefault();
-
-        if(newTodoValue === ''){
-            setOpenModal(false)
-        }else{
-            addTodo(newTodoValue, newCategoryValue)
-            setOpenModal(false)
-        }
+    if (newTodoValue === "") {
+      setOpenModal(false);
+    } else {
+      addTodo(newTodoValue, newCategoryValue);
+      setOpenModal(false);
     }
-    const onCancel = (event) =>{
-        event.preventDefault();
-        setOpenModal(false)
+  };
+  const onCancel = (event) => {
+    event.preventDefault();
+    setOpenModal(false);
+  };
+  const onChangeCategory = (event) => {
+    const arrayCategories = event.target.value.split(",");
+    if (!arrayCategories.includes(" ")) {
+      setNewCategoryValue(arrayCategories);
     }
-    const onChangeCategory = (event) =>{
-        setNewCategoryValue(event.target.value)
-    }
-    const onChangeTodo= (event) =>{
-        setNewTodoValue(event.target.value);
-    }
+  };
 
+  const onChangeTodo = (event) => {
+    setNewTodoValue(event.target.value);
+  };
+  const addCategory = (event) =>{
+    event.preventDefault()
+    console.log(newCategoryValue.length)
+    const newCategories = CategoriesList
+    if(newCategoryValue){
+        newCategories.unshift([...newCategoryValue])
+        setCategoriesList(newCategories)
+    }
+    setNewCategoryValue('')
 
-    return(
-        <form onSubmit = {onSubmit}>
-            <label>Escribe tu nuevo TODO</label>
-            <textarea
-                placeholder = "Estudiar Full Stack Development"
-                value={newTodoValue}
-                onChange={onChangeTodo}
-            />
-            <textarea
-                onKeyUpCapture={DetectEvents}
-                placeholder = "Category"
-                value={newCategoryValue}
-                onChange={onChangeCategory}
-            />   
-                {isTodoTag && <TodoTag text={newCategoryValue}/>}
-            
-            
-            <div className="TodoForm-buttonContainer"></div>
-            <button type ="button" className = "TodoForm-button TodoForm-button TodoForm-button--cancel" onClick = {onCancel}>
-                Cancelar
-            </button>
-            <button type="submit" className = "TodoForm-button TodoForm-button--add" >
-                Añadir TODO
-            </button>
+    setTodoTag(false)
+  }
+  useEffect(() => {
+    console.log("Categories List", CategoriesList);
+  }, [CategoriesList]);
+
+  return (
+    <>
+      <section>
+        <form onSubmit={onSubmit}>
+          <label>Escribe tu nuevo TODO</label>
+          <textarea
+            placeholder="Estudiar Full Stack Development"
+            value={newTodoValue}
+            onChange={onChangeTodo}
+          />
+
+          <div className="TodoForm-buttonContainer"></div>
+          <button
+            type="button"
+            className="TodoForm-button TodoForm-button TodoForm-button--cancel"
+            onClick={onCancel}
+          >
+            Cancelar
+          </button>
+          <button
+            type="submit"
+            className="TodoForm-button TodoForm-button--add"
+          >
+            Añadir TODO
+          </button>
         </form>
-    );
+        <div className="container">
+      <textarea
+        onKeyUpCapture={DetectEvents}
+        placeholder="Category"
+        value={newCategoryValue}
+        onChange={onChangeCategory}
+        className="input"
+      />
+      <button className="button" onClick={addCategory}>Add category</button>
+      {isTodoTag && newCategoryValue.map((category, index) => (
+        <TodoTag key={index} text={category} index={parseInt(index) + 1} />
+      ))}
+      {CategoriesList.length > 0 && CategoriesList.map((category, index) => (
+        <TodoTag key={index} text={category} index={parseInt(index) + 1} />
+      ))}
+    </div>
+      </section>
+    </>
+  );
 }
 
-export {TodoForm}
+
+
+
+export { TodoForm };
+
+// <textarea
+// onKeyUpCapture={DetectEvents}
+// placeholder = "Category"
+// value={newCategoryValue}
+// onChange={onChangeCategory}
+// />
+// {isTodoTag && newCategoryValue.map((category,index)=>(
+//     <TodoTag text={category} index={parseInt(index)+1}/>
+// ))}
--- src/components/TodoTag/index.jsx
+++ src/components/TodoTag/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './TodoTag.css'; // Make sure to create this CSS file and include it in your project
 
-function TodoTag({ text }) {
+function TodoTag({ text,index }) {
     console.log(text)
     return (
-        <span className="todo-tag">
+        <span className="todo-tag" onClick={() =>alert ("editar elemento"+ index )}>
             {text}
             <i className="fas fa-times todo-tag-remove" ></i>
         </span>
