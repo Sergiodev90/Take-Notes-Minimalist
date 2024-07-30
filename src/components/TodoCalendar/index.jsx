1153d0e0af7495ab383d19078b7ecff51dc446fb
sergio noriega cantillo <sergionoriegacantillo@gmail.com>
2024-07-30 17:46:48 +0200

    finish to style and configure the component todocalendar

--- src/components/TodoCalendar/index.jsx
+++ src/components/TodoCalendar/index.jsx
@@ -3,8 +3,10 @@ import Calendar from 'react-calendar';
 import DatePicker from 'react-datepicker';
 import 'react-calendar/dist/Calendar.css';
 import 'react-datepicker/dist/react-datepicker.css';
-import './TodoCalendar.css'
-function TodoCalendar (){
+import { addDays } from 'date-fns';
+import './TodoCalendar.css';
+
+function TodoCalendar() {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [selectedDate, setSelectedDate] = useState(new Date());
@@ -13,44 +15,49 @@ function TodoCalendar (){
         setSelectedDate(date);
         setStartDate(date);
         setEndDate(date);
-      };
-      return (
+    };
+
+    return (
         <div className='container'>
-          <div className="calendarContainer">
-            <Calendar
-              onChange={handleDateChange}
-              value={selectedDate}
-            />
-          </div>
-          <div className="datePickerContainer">
-            <div className="dateTimePicker">
-              <label>Fecha de inicio:</label>
-              <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                showTimeSelect
-                timeFormat="HH:mm"
-                timeIntervals={15}
-                timeCaption="Hora"
-                dateFormat="MMMM d, yyyy h:mm aa"
-              />
+            <div className="calendarContainer">
+                <Calendar
+                    onChange={handleDateChange}
+                    value={selectedDate}
+                    minDate={new Date()}
+                />
             </div>
-            <div className="dateTimePicker">
-              <label>Fecha de fin:</label>
-              <DatePicker
-                selected={endDate}
-                onChange={(date) => setEndDate(date)}
-                minDate={startDate}
-                showTimeSelect
-                timeFormat="HH:mm"
-                timeIntervals={15}
-                timeCaption="Hora"
-                dateFormat="MMMM d, yyyy h:mm aa"
-              />
+            <div className="datePickerContainer">
+                <div className="dateTimePicker">
+                    <label>Fecha de inicio:</label>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        timeCaption="Hora"
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                        minDate={new Date()}
+                        className="datePickerInput"
+                    />
+                </div>
+                <div className="dateTimePicker">
+                    <label>Fecha de fin:</label>
+                    <DatePicker
+                        selected={endDate}
+                        onChange={(date) => setEndDate(date)}
+                        minDate={startDate}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        timeCaption="Hora"
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                        className="datePickerInput"
+                    />
+                </div>
             </div>
-          </div>
         </div>
-      );
+    );
 }
 
-export {TodoCalendar}
+export { TodoCalendar };
